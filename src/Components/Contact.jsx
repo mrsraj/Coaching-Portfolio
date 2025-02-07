926e37446bcba078ec13fcaf00fd8ca8a5fe49ef
Sachin Raj <sachinraj6392@gmail.com>
2025-02-07 21:12:50 +0530

    modify contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,6 @@ import "../CSS/Contact.css";
 
 const Contact = () => (
     <section id="contact" className="contact">
-        <h2>Contact</h2>
         <p>Email: raj@example.com</p>
         <p>Phone: +91 123456-7890</p>
     </section>
