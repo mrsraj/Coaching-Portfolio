e63f7f3b55855941e7239e519792103a6b856ead
Sachin Raj <sachinraj6392@gmail.com>
2025-02-19 21:36:06 +0530

    Add Link

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,24 +5,21 @@ import Contact from "./Contact";
 const Footer = () => (
     <footer className={style.footer} id="contact">
         <div>
-            <h4>INSTAGRAM</h4>
+            <h4><a className={style.INSTAGRAM} href="https://www.instagram.com/">INSTAGRAM</a></h4>
         </div>
 
         <div>
-            <h4>FACEBOOK</h4>
+            <h4><a className={style.FACEBOOK} href="https://www.facebook.com/">FACEBOOK</a></h4>
         </div>
 
         <div>
-            <h4>LINKEDIN</h4>
+            <h4><a className={style.LINKEDIN} href="https://www.linkedin.com/">LINKEDIN</a></h4>
         </div>
         <div>
             <h4>CONTACT</h4>
             <Contact />
         </div>
-
-        <div>
-            <a href="https://wa.me/7322078774?text-Hello am Sachin Raj?" target="_blank">w</a>
-        </div>
+        
     </footer>
 );
 
