52292fdde58c2640cdfe4dce890e978a511d786b
Sachin Raj <sachinraj6392@gmail.com>
2025-02-19 21:59:16 +0530

    Add CSS

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,6 +5,7 @@ const Contact = () => (
     <section id="contact" className="contact">
         <p>Email: raj@example.com</p>
         <p>Phone: +91 123456-7890</p>
+        <p><a className="whatsapp" href="https://wa.me/7322078774?text-Hello am Sachin Raj?" target="_blank">WhatsApp</a></p>
     </section>
 );
 
